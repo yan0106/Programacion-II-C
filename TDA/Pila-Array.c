40498275690a7d7b51021e02e0243cb6c6ca3c14
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-24 16:56:19 -0300

    Agregar prototipos de funciones

---
+++ TDA/Pila-Array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define CAPACIDAD 15
+
+void apila(int); // 
+int desapila();
+void vaciaPila();
+int tamaño();
+int estaVacia();
+int cima();
+
+int main () {
+
+    int elementos [CAPACIDAD];
+    int cima = -1;
+
+
+
+}
