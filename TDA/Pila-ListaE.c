4eed8829dfdc8df369d92be893d0dd8849794c45
Yandira Diaz <yandiradiaz@hotmail.com>
2026-03-18 17:16:46 -0300

    Agregar puntero a pila

--- TDA/Pila-ListaE.c
+++ TDA/Pila-ListaE.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Pila-ListaE.h"
+
+/*completar las funciones prototipadas en el .h*/
+
+int main () {
+
+    pila *cima;
+    cima = NULL;
+
+    /*armar código de main para probar la pila*/
+
+    return 0;
+}
+
+
+
