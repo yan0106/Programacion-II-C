259798d9279c2621c4a357b67ceac5f6d051c42a
Yandira Diaz <yandiradiaz@hotmail.com>
2026-07-04 17:56:10 -0300

    Agregar desarrollo f ingresaProceso

--- TP Final/TP_Final.c
+++ TP Final/TP_Final.c
@@ -3,10 +3,9 @@
 
 // 0 1 2 3 4 5 6 7 8 9 
 
-// Tenga en cuenta los siguientes prototipos:
+static int id = 0; // Contador de prioridad
 
-static int id = 0;
-
+/* Estructura proceso */
 typedef struct proceso{
     int procesador;
     int id_proceso;
@@ -14,12 +13,50 @@ typedef struct proceso{
     char estado[MAX];
 } proceso;
 
-proceso * scheduling[MAX]; // Arreglo estático de 10 posiciones de punteros a estructura proceso
+/* Arreglo estático de 10 posiciones de punteros a estructuras proceso. 
+Reserva memoria para 10 punteros. Pone los 10 casilleros en NULL */
+proceso * scheduling[MAX] = {NULL};
 
 /* Asigna el siguiente estado según el orden enunciado antes */
 void asignaEstado(proceso*);
+
 /* Ingresa el proceso al Scheduler en el primer espacio libre que encuentre*/
-void ingresaProceso();
+void ingresaProceso() {
+    static int posicion_actual = 0; // inicio con posición en 0
+    int ingresado = 0; // bandera
+    for (int i = 0; i < MAX; i++) {
+        if (scheduling[posicion_actual] == NULL) {
+            //1. si la posición es vacía, reserva memoria para el proceso
+            scheduling[posicion_actual] = malloc(sizeof(struct proceso));
+
+            //2. asigna datos...(continuar)
+
+            //3. incrementa la posición
+            posicion_actual++;
+            //4. chequea que no se pase de MAX
+            if (posicion_actual == MAX) {
+                posicion_actual = 0; // si posicion_actual llega a MAX, pega la vuelta a 0
+            };
+            //5. asigna 1 a la bandera
+            ingresado = 1;
+            //6. frena el bucle
+            break;
+        } else {
+            // si el casillero actual está ocupado, también avanza la posicion_actual para que en la 
+            // próxima vuelta del for, revise el que sigue
+            posicion_actual++;
+            // vuelve a chequear que no se pase de MAX
+            if (posicion_actual == MAX) {
+                posicion_actual = 0;
+            };
+        };
+    };
+    // si salí del for e 'ingresado' sigue en 0, es porque nunca encontró NULL (todo lleno)
+    if (ingresado == 0) {
+        printf ("No es posible ingresar el proceso. Scheduling lleno.\n");
+    };
+}
+
 /*Quita el proceso de la cola liberando y retornando el lugar liberado*/
 int terminaProceso();
 /*Recorrera todos los procesos de la cola, haciendo el cambio del estado de los mismos*/
